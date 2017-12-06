69fc650c1ca603e65f3aef9afff34c24088c7f78
11111 <11111@qq.com>
2017-12-06 18:54:07 +0800

    bug分支修改

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-#sdfghj12345
+#sdfghj12345------修复bug
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "a01.settings")
     try:
